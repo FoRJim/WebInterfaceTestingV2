272f65bade2dda38155fcdf0b45eeccb17f0792d
“Vasiliy <vasenka_kuznetsov_1997@mail.ru>
2023-03-22 23:47:21 +0300

    fix_code2

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -20,12 +20,12 @@ public class CardOrderTest {
     }
     @BeforeEach
     void setUp() {
-        //ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--disable-dev-shm-usage");
-        //options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
-        //driver = new ChromeDriver(options);
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        //driver = new ChromeDriver();
 
     }
 
